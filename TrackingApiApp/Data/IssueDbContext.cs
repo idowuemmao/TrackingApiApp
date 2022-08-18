e4ef3622c341cfc68480aeef3c9c52231bedf30c
USER <USER@EMMYGREAT>
2022-08-18 11:17:29 +0100

    Add project files.

---
+++ TrackingApiApp/Data/IssueDbContext.cs
@@ -0,0 +1,14 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using TrackingApiApp.Models;
+
+namespace TrackingApiApp.Data;
+
+public class IssueDbContext : DbContext 
+{
+    public IssueDbContext(DbContextOptions<IssueDbContext> options)
+        :base(options)
+    {
+    }
+    public DbSet<Issue> Issues { get; set; }
+}
---
+++ TrackingClient/Program.cs
@@ -0,0 +1,26 @@
+﻿using static System.Console;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using TrackingClient;
+
+HttpClient client = new();
+client.BaseAddress = new Uri("https://localhost:7088");
+client.DefaultRequestHeaders.Accept.Clear();
+client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+HttpResponseMessage response = await client.GetAsync("api/issue");
+response.EnsureSuccessStatusCode();
+
+if (response.IsSuccessStatusCode)
+{
+    var issues = await response.Content.ReadFromJsonAsync <IEnumerable<IssueDto>>();
+    foreach (var issue in issues)
+    {
+        WriteLine(issue.Title);
+    }
+
+}
+else
+{
+    WriteLine("No Results");
+}
